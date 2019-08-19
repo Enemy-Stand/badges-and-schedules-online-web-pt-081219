e7a71577aba9d4bb70339b9f36f4dcd931de2540
Joseph Sesti <sestijoey63@gmail.com>
2019-08-19 23:46:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,15 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.collect {|attendee| badges.push("Hello, my name is #{attendee}.")}
-  #return badges
+  attendees = []
+  badges = []
+  array.each {|attendee| badges.push("Hello, my name is #{attendee}.")}
+  return badges
 end
 
 def assign_rooms(array)
+  attendees = []
+  room_assignments = []
   array.each_with_index {|attendee, index| room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index+1}!")}
   return room_assignments
 end
