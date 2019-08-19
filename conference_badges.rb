ab5b7ec3d33dbfaac1db715bf7a8f887b0218dad
Joseph Sesti <sestijoey63@gmail.com>
2019-08-19 23:48:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,3 +15,7 @@ def assign_rooms(array)
   array.each_with_index {|attendee, index| room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{index+1}!")}
   return room_assignments
 end
+
+def printer(array)
+  attendees = []
+  
