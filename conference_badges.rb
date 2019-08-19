2d6297f26d55a355c418f341a07e30c6c02fe561
Joseph Sesti <sestijoey63@gmail.com>
2019-08-19 22:50:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,6 @@ end
 def batch_badge_creator(array)
   attendees = []
   badges = []
-  array.each {|attendee| badges.push(attendees)}
+  array.each {|attendees| badges.push(attendee)}
   return badges
 end
